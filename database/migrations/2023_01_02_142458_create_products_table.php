af3fa213f6f12c0835da785bb91937b2c8602370
Bandana <gbandana21@tbc.edu.np>
2023-01-02 22:04:59 +0545

    Changed values

--- database/migrations/2023_01_02_142458_create_products_table.php
+++ database/migrations/2023_01_02_142458_create_products_table.php
@@ -16,8 +16,8 @@ return new class extends Migration
     Schema::create('products', function (Blueprint $table) {
         $table->id();
   	    $table->timestamps();
-        $table->string('artist,255');
-  	    $table->string('title,255');
+        $table->string('artist');
+  	    $table->string('title');
         $table->unsignedInteger("price");
       
     });
