05d9af4770295b7f2199edf4da06264303fbc316
Bandana <gbandana21@tbc.edu.np>
2023-01-07 14:54:03 +0545

    added the delete function

--- Final component/app/Http/Controllers/ProductController.php
+++ Final component/app/Http/Controllers/ProductController.php
@@ -102,6 +102,11 @@ class ProductController extends Controller
      */
     public function destroy(product $product)
     {
-        //
+        //delete the product
+        $product->delete();
+
+
+        //redirect the user and display a success message
+        return redirect()->route('products.index')->with('success', 'Product deleted successfully');
     }
 }
