2edd53319f4082069ba0e938d2218686d2009050
Bandana <gbandana21@tbc.edu.np>
2023-01-07 13:14:27 +0545

    Added laravel project and created product controller

---
+++ Final component/resources/views/products/layout.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Final project laravel</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha/css/bootstrap.css" rel="stylesheet">
+</head>
+<body>
+
+<h1>Final project laravel</h1>
+
+<div class="container">
+    <br>
+    @yield('content')
+</div>
+
+</body>
+</html>
