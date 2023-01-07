f5353ef22749a315edafe22ef721b6193b83a84f
Bandana <gbandana21@tbc.edu.np>
2023-01-07 20:59:22 +0545

    Seeded random data with factory & faker

--- Final component/database/migrations/2023_01_06_123511_create_products_table.php
+++ Final component/database/migrations/2023_01_06_123511_create_products_table.php
@@ -16,7 +16,6 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
             $table->text('detail');
-            $table->text('price');
             $table->timestamps();
         });
     }
---
+++ Final component/database/seeders/ProductSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $product = new \App\Models\Product([
+            'name' => 'Product #1',
+            'detail' => 'Product #1 details'
+
+        ]);
+        $product->save();
+    }
+}
